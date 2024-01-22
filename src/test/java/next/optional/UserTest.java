cca1f46c0bc9df39068f000ce75e24f447a7bb4d
99winnmin <ojysep9987@gmail.com>
2024-01-22 16:38:50 +0900

    :sparkles: Feat : 요구사항 5 - 인자를 가진 생성자의 인스턴스 생성
    "요구 사항 : User 클래스의 인스턴스를 자바 Reflection API를 활용해 User 인스턴스를 생성한다."

--- src/test/java/next/optional/UserTest.java
+++ src/test/java/next/optional/UserTest.java
@@ -1,10 +1,14 @@
 package next.optional;
 
 
-import org.junit.jupiter.api.Test;
-
-import static next.optional.User.*;
+import static next.optional.User.ageIsInRange1;
+import static next.optional.User.ageIsInRange2;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class UserTest {
     @Test
@@ -23,5 +27,20 @@ public class UserTest {
         assertThat(ageIsInRange2(new User("crong", null))).isFalse();
         assertThat(ageIsInRange2(new User("crong", 29))).isFalse();
         assertThat(ageIsInRange2(null)).isFalse();
+    }
+
+    @Test
+    @DisplayName("테스트1: User 클래스의 인스턴스를 자바 Reflection API를 활용해 User 인스턴스를 생성한다.")
+    public void createInstance() throws Exception {
+        Class<User> clazz = User.class;
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+
+        for (Constructor<?> constructor : constructors) {
+            Parameter[] parameters = constructor.getParameters();
+            Object[] parameterTypes = new Object[parameters.length];
+
+            assertThat(clazz)
+                .isEqualTo(constructor.newInstance(parameterTypes).getClass());
+        }
     }
 }
