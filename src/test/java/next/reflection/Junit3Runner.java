47edfff43a98168d2ddb87988960014eb16df279
99winnmin <ojysep9987@gmail.com>
2024-01-22 16:36:56 +0900

    :sparkles: Feat : 요구사항 2 - test로 시작하는 메소드 실행
    "요구 사항 : Junit3에서는 test로 시작하는 메소드를 자동으로 실행한다. 이와 같이 Junit3Test 클래스에서 test로 시작하는 메소드만 Java Reflection을 활용해 실행하도록 구현한다.
     구현은 src/test/java > next.reflection > Junit3Runner 클래스의 runner() 메소드에 한다."

--- src/test/java/next/reflection/Junit3Runner.java
+++ src/test/java/next/reflection/Junit3Runner.java
@@ -1,11 +1,19 @@
 package next.reflection;
 
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class Junit3Runner {
     @Test
+    @DisplayName("테스트1: test 로 시작하는 메소드를 자동으로 실행한다.")
     public void runner() throws Exception {
-        Class clazz = Junit3Test.class;
-
+        Class<Junit3Test> clazz = Junit3Test.class;
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
     }
 }
