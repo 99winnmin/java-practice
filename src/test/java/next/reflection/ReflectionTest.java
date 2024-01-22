000b48c438961e6e7916071cccc5fd1ba34e16ac
99winnmin <ojysep9987@gmail.com>
2024-01-22 16:35:51 +0900

    :sparkles: Feat : 요구사항 1 - 클래스 정보 출력
    "요구 사항 : src/test/java > next.reflection > ReflectionTest의 showClass() 메소드를 구현해 Question 클래스의 모든 필드, 생성자, 메소드에 대한 정보를 출력한다."

--- src/test/java/next/reflection/ReflectionTest.java
+++ src/test/java/next/reflection/ReflectionTest.java
@@ -1,35 +1,66 @@
 package next.reflection;
 
 import java.lang.reflect.Constructor;
-
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import static org.assertj.core.api.Assertions.*;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
     @Test
     @DisplayName("테스트1: 리플렉션을 이용해서 클래스와 메소드의 정보를 정확하게 출력해야 한다.")
-    public void showClass() {
+    public void showClass() throws Exception {
         SoftAssertions s = new SoftAssertions();
         Class<Question> clazz = Question.class;
-        logger.debug("Classs Name {}", clazz.getName());
+        logger.debug("Class Name {}", clazz.getName());
     }
 
     @Test
+    @DisplayName("테스트2: 리플렉션을 이용해서 클래스의 생성자 정보를 출력해야 한다.")
     public void constructor() throws Exception {
         Class<Question> clazz = Question.class;
         Constructor[] constructors = clazz.getConstructors();
         for (Constructor constructor : constructors) {
+            StringBuilder sb = new StringBuilder();
             Class[] parameterTypes = constructor.getParameterTypes();
-            logger.debug("paramer length : {}", parameterTypes.length);
+            sb.append("\nconstructor name : ").append(constructor.getName()).append("\n");
+            sb.append("parameter length : ").append(parameterTypes.length).append("\n");
             for (Class paramType : parameterTypes) {
-                logger.debug("param type : {}", paramType);
+                sb.append("param type : ").append(paramType).append("\n");
             }
+            logger.debug(sb.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("테스트3: 리플렉션을 이용해서 클래스의 필드 정보를 출력해야 한다.")
+    public void field() {
+        Class<Question> clazz = Question.class;
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            logger.debug("\nfield name : {} \nfield type : {}", field.getType(), field.getName());
+        }
+    }
+
+    @Test
+    @DisplayName("테스트4: 리플렉션을 이용해서 클래스의 메소드 정보를 출력해야 한다.")
+    public void method() throws Exception {
+        Class<Question> clazz = Question.class;
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("\nmethod name : ").append(method.getName()).append("\n");
+            Class[] parameterTypes = method.getParameterTypes();
+            for (Class paramType : parameterTypes) {
+                sb.append("param type : ").append(paramType).append("\n");
+            }
+            sb.append("return type : ").append(method.getReturnType()).append("\n");
+            logger.debug(sb.toString());
         }
     }
 }
