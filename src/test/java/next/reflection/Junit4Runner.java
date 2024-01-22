5fd054b9d17991f25da8913fdb26457dcf16f16f
99winnmin <ojysep9987@gmail.com>
2024-01-22 16:37:33 +0900

    :sparkles: Feat : 요구사항 3 - @Test 애노테이션 메소드 실행
    "요구 사항 : Junit4에서는 @Test 애노테이션일 설정되어 있는 메소드를 자동으로 실행한다.
    이와 같이 Junit4Test 클래스에서 @MyTest 애노테이션으로 설정되어 있는 메소드만 Java Reflection을 활용해 실행하도록 구현한다.
    구현은 src/test/java > next.reflection > Junit4TestRunner 클래스의 run() 메소드에 한다."

--- src/test/java/next/reflection/Junit4Runner.java
+++ src/test/java/next/reflection/Junit4Runner.java
@@ -1,12 +1,20 @@
 package next.reflection;
 
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class Junit4Runner {
     @Test
+    @DisplayName("테스트1: @Test 애노테이션일 설정되어 있는 메소드를 자동으로 실행한다.")
     public void run() throws Exception {
-        Class clazz = Junit4Test.class;
-
+        Class<Junit4Test> clazz = Junit4Test.class;
+        Method[] declaredMethods = clazz.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
     }
 }
 
